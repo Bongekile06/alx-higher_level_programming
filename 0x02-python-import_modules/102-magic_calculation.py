567616f6c2fd8d846ffef6d4ee2b25e2e368c202
Bongekile06 <mlindwab@gmail.com>
2023-09-07 03:24:58 -0700

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    add, sub = (None, None)
 
     if a < b:
+        add, sub = (lambda x, y: x + y, lambda x, y: x - y)
         c = add(a, b)
 
         for i in range(4, 6):
             c = add(c, i)
 
         return c
+    else:
+        sub = lambda x, y: x - y
+        return sub(a, b)
 
-    return sub(a, b)
