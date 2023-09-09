fb83c9b7f022fbe9f7c4439acec5f8a9aa38c346
Bongekile06 <mlindwab@gmail.com>
2023-09-09 07:51:01 -0700

    fix palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,12 +1,9 @@
 /*
  * File: 13-is_palindrome.c
- * Auth: Bongekile Mlindwa
+ * Auth: BONGEKILE MLINDWA
  */
 
 #include "lists.h"
-
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
 
 /**
  * reverse_listint - Reverses a singly-linked listint_t list.
@@ -53,7 +50,7 @@ int is_palindrome(listint_t **head)
 	}
 
 	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
+	for (i = 0; i < size / 2; i++)  /* Updated loop condition */
 		tmp = tmp->next;
 
 	if ((size % 2) == 0 && tmp->n != tmp->next->n)
